b6011b57e5ab6c27ec312b69485c7c1694736b4b
Blue-Flag-666 <64064866+Blue-Flag-666@users.noreply.github.com>
2023-02-04 17:36:46 +0800

    Write files info to bgscripts.json

--- src/scanv2.js
+++ src/scanv2.js
@@ -1,5 +1,4 @@
 import core from '@actions/core';
-import crypto from 'node:crypto';
 import fs from 'node:fs';
 import path from 'node:path';
 import {request} from 'undici';
@@ -8,86 +7,84 @@ const STARTING_VERSION = 0;
 const ENDING_VERSION = 3000;
 const SUB_ID_MAX = 10;
 
+let json_path = path.join('bgscripts', 'bgscripts.json');
+let json_arr_old = [];
+let json_arr = [];
+
 async function get_script_sub(version, subId)
 {
 	const RPF_URL = `https://prod.cloud.rockstargames.com/titles/gta5/pcros/bgscripts/bg_ng_${version}_${subId}.rpf`;
 
-	try
+	const response = await request(RPF_URL);
+	if (response.statusCode !== 200)
 	{
-		const response = await request(RPF_URL);
-		if (response.statusCode !== 200)
+		if (response.statusCode !== 404)
 		{
-			if (response.statusCode !== 404)
-			{
-				console.log(`Failed to fetch bgscript: ${response.statusCode} @ ${RPF_URL}`);
-			}
-			return;
+			core.setFailed(`Failed to fetch bgscript: ${response.statusCode} @ ${RPF_URL}`);
 		}
-
-		const bgScriptName = `bg_ng_${version}_${subId}`;
-
-		console.log(`Found bgscript @ ${RPF_URL}`);
-
-		if (!fs.existsSync('bgscripts'))
-		{
-			fs.mkdirSync('bgscripts');
-		}
-		const rpfPath = path.join('bgscripts', bgScriptName);
-		if (!fs.existsSync(rpfPath))
-		{
-			fs.mkdirSync(rpfPath);
-		}
-		if (!fs.existsSync(path.join(rpfPath, 'last-modified.txt')))
-		{
-			fs.writeFileSync(path.join(rpfPath, 'last-modified.txt'), 'Thu, 01 Jan 1970 00:00:00 GMT');
-		}
-
-		if (response.headers['content-type'] !== 'application/octet-stream')
-		{
-			core.setFailed(`${bgScriptName}: Invalid content-type: ${response.headers['content-type']}`);
-			return;
-		}
-
-		const lastModified = response.headers['last-modified'];
-		const lastModifiedDate = new Date(lastModified);
-
-		if (lastModifiedDate.toString() === 'Invalid Date')
-		{
-			core.setFailed(`${bgScriptName}: Invalid last-modified date: ${lastModified}`);
-			return;
-		}
-
-		const lastModifiedFile = path.join(rpfPath, 'last-modified.txt');
-		const lastModifiedFileDate = new Date(fs.readFileSync(lastModifiedFile, 'utf8'));
-
-		if (lastModifiedFileDate.toString() === 'Invalid Date')
-		{
-			core.setFailed(`${bgScriptName}: Invalid last-modified date in file: ${lastModifiedFileDate}`);
-			return;
-		}
-
-		if (lastModifiedDate.getTime() === lastModifiedFileDate.getTime())
-		{
-			core.info(`${bgScriptName}: bgscript is up to date`);
-			return;
-		}
-
-		core.info(`${bgScriptName}: New bgscript available`);
-
-		// get content as buffer
-		const buffer = Buffer.from(await response.body.arrayBuffer());
-
-		const fileNameDate = lastModifiedDate.toISOString().replaceAll(':', '-');
-		const fileName = `${fileNameDate}-${crypto.createHash('sha256').update(buffer).digest('hex')}.rpf`;
-
-		fs.writeFileSync(path.join(rpfPath, fileName), buffer);
-
-		fs.writeFileSync(lastModifiedFile, lastModified);
+		return;
 	}
-	catch (error)
+
+	const bgScriptName = `bg_ng_${version}_${subId}`;
+
+	if (!fs.existsSync('bgscripts'))
 	{
-		console.log(error);
+		fs.mkdirSync('bgscripts');
 	}
+	const rpfPath = path.join('bgscripts', bgScriptName);
+	if (!fs.existsSync(rpfPath))
+	{
+		fs.mkdirSync(rpfPath);
+	}
+	if (!fs.existsSync(path.join(rpfPath, 'last-modified.txt')))
+	{
+		fs.writeFileSync(path.join(rpfPath, 'last-modified.txt'), 'Thu, 01 Jan 1970 00:00:00 GMT');
+	}
+
+	if (response.headers['content-type'] !== 'application/octet-stream')
+	{
+		core.setFailed(`${bgScriptName}: Invalid content-type: ${response.headers['content-type']}`);
+		return;
+	}
+
+	const lastModified = response.headers['last-modified'];
+	const lastModifiedDate = new Date(lastModified);
+
+	if (lastModifiedDate.toString() === 'Invalid Date')
+	{
+		core.setFailed(`${bgScriptName}: Invalid last-modified date: ${lastModified}`);
+		return;
+	}
+
+	const lastModifiedFile = path.join(rpfPath, 'last-modified.txt');
+	const lastModifiedFileDate = new Date(fs.readFileSync(lastModifiedFile, 'utf8'));
+
+	if (lastModifiedFileDate.toString() === 'Invalid Date')
+	{
+		core.setFailed(`${bgScriptName}: Invalid last-modified date in file: ${lastModifiedFileDate}`);
+		return;
+	}
+
+	if (lastModifiedDate.getTime() === lastModifiedFileDate.getTime())
+	{
+		core.info(`${bgScriptName}: Script is up to date`);
+		return;
+	}
+
+	core.info(`New script available at ${RPF_URL}`);
+
+	// get content as buffer
+	const buffer = Buffer.from(await response.body.arrayBuffer());
+
+	const fileNameDate = lastModifiedDate.toISOString().replaceAll(':', '-');
+	const fileName = `${bgScriptName}-${fileNameDate}.rpf`;
+	const filePath = path.join(rpfPath, fileName);
+
+	fs.writeFileSync(filePath, buffer);
+
+	fs.writeFileSync(lastModifiedFile, lastModified);
+
+	json_arr.push({name: bgScriptName, files: [filePath]});
 }
 
 function get_script(version)
@@ -102,18 +99,57 @@ function get_script(version)
 
 async function main()
 {
-	console.log(`Fetching start`);
+	if (fs.existsSync(json_path))
+	{
+		try
+		{
+			json_arr_old = JSON.parse(fs.readFileSync(json_path, 'utf8'));
+		}
+		catch (e)
+		{
+			core.info('Bad Json format');
+			json_arr_old = [];
+		}
+	}
 
-	let last = 0;
+	core.info('Fetching start');
+	let last = STARTING_VERSION;
 	for (let version = STARTING_VERSION; version <= ENDING_VERSION; version++)
 	{
 		await Promise.all(get_script(version));
 		if (version !== last && version % 100 === 0)
 		{
-			console.log(`Fetching version ${last} - ${version} finished`);
+			core.info(`Fetching version ${last} - ${version} finished`);
 			last = version;
 		}
 	}
 }
 
-main().then(() => console.log(`Fetching Scripts Finished`));
+main().then(() =>
+            {
+	            core.info('Fetching scripts finished');
+
+	            let script_map = new Map();
+	            for (let obj of json_arr_old)
+	            {
+		            script_map.set(obj.name, obj.files);
+	            }
+	            for (let obj of json_arr)
+	            {
+		            let set = new Set(script_map.get(obj.name));
+		            for (let file of obj.files)
+		            {
+			            set.add(file);
+		            }
+		            if (!script_map.get(obj.name))
+		            {
+			            script_map.set(obj.name, []);
+		            }
+		            script_map.set(obj.name, Array.from(set));
+	            }
+
+	            let json = [];
+	            script_map.forEach((value, key) => json.push({name: key, files: value}));
+
+	            fs.writeFileSync(json_path, JSON.stringify(json));
+            });
